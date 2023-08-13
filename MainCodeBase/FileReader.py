a2db9f62c201a712e4d58cba65e4cb671a6a1025
bombard21 <wangxiaofeng153@gmail.com>
2023-08-13 21:31:30 +1200

    Moved To Main Created a file called main.py and called all the functions from the other files to run in this file

--- MainCodeBase/FileReader.py
+++ MainCodeBase/FileReader.py
@@ -31,14 +31,3 @@ def print_conversation_data(Sentences):
             print(f"Line {index}:", text)
         print()
 
-
-def main():
-    csv_filename = "../Sentiment-Analysis-Main/AnnotatedDataSets/Tower_Sentiment.xlsx"
-    
-    df = read_csv(csv_filename)
-    Sentences = store_conversation_data(df)
-    print_conversation_data(Sentences)
-
-
-if __name__ == "__main__":
-    main()
